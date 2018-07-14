e5faea25de13e260613e424809e5ffbcef4f2bc6
Horace Lee <horacehylee2@gmail.com>
2018-07-14 19:06:20 +0800

    locally define regex

--- src/modules/yt-dom-selector/selectVideo.ts
+++ src/modules/yt-dom-selector/selectVideo.ts
@@ -1,10 +1,8 @@
 import * as select from "select-dom";
 
-const hasNumberAsFirstCharPattern = new RegExp('^[0-9]')
-
 export const selectVideo = (videoId: string): HTMLAnchorElement => {
   let _videoId = videoId;
-  if (hasNumberAsFirstCharPattern.test(_videoId)) {
+  if (/^[0-9]/.test(_videoId)) {
     _videoId = _videoId.substring(1);
   }
   return select(`a[href*=${_videoId}][id=thumbnail]`);
