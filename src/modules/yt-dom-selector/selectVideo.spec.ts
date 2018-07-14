7666a11d5a5de6784a8459265067553bdca1aa31
Horace Lee <horacehylee2@gmail.com>
2018-07-14 16:04:46 +0800

    fix video element selector bug when video id has number as its first character

--- src/modules/yt-dom-selector/selectVideo.spec.ts
+++ src/modules/yt-dom-selector/selectVideo.spec.ts
@@ -3,28 +3,45 @@ import { promisify } from "bluebird";
 import { selectVideo } from "./selectVideo";
 const readFileAsync = promisify(readFile);
 
-describe("selectVideo", () => {
-  it("should select videoId element", async () => {
-    const buffer = await readFileAsync(
-      __dirname + "/../../../sample/ytVideoElement.html"
-    );
-    document.body.innerHTML = buffer.toString();
-    const element = selectVideo("sLgKXX0FrJo");
+interface SelectVideoTestOption {
+  description: string;
+  sampleFileName: string;
+  videoId: string;
+}
 
-    expect(element).toBeDefined();
-    expect(element).toBeInstanceOf(HTMLAnchorElement);
-    expect(element).toMatchSnapshot();
+const buildTests = (options: SelectVideoTestOption[]) => {
+  return options.map(({ description, sampleFileName, videoId }) => {
+    it(description, async () => {
+      const buffer = await readFileAsync(
+        `${__dirname}/../../../sample/${sampleFileName}`
+      );
+      document.body.innerHTML = buffer.toString();
+      const element = selectVideo(videoId);
+
+      expect(element).toBeDefined();
+      expect(element).toBeInstanceOf(HTMLAnchorElement);
+      expect(element).toMatchSnapshot();
+    });
   });
+};
 
-  it('should select video element with time value in url', async() => {
-    const buffer = await readFileAsync(
-      __dirname + "/../../../sample/ytVideoElement_withTime.html"
-    );
-    document.body.innerHTML = buffer.toString();
-    const element = selectVideo("xu6KLURIuh8");
-
-    expect(element).toBeDefined();
-    expect(element).toBeInstanceOf(HTMLAnchorElement);
-    expect(element).toMatchSnapshot();
-  })
+describe("selectVideo", () => {
+  buildTests([
+    {
+      description: "should select videoId element",
+      sampleFileName: "ytVideoElement.html",
+      videoId: "sLgKXX0FrJo"
+    },
+    {
+      description: "should select video element with time value in url",
+      sampleFileName: "ytVideoElement_withTime.html",
+      videoId: "xu6KLURIuh8"
+    },
+    {
+      description:
+        "should select video element with number as the first character",
+      sampleFileName: "ytVideoElement_numberAsFirstChar.html",
+      videoId: "2LifjpxpIwU"
+    }
+  ]);
 });
--- src/modules/yt-dom-selector/selectVideo.ts
+++ src/modules/yt-dom-selector/selectVideo.ts
@@ -1,5 +1,11 @@
 import * as select from "select-dom";
 
+const hasNumberAsFirstCharPattern = new RegExp('^[0-9]')
+
 export const selectVideo = (videoId: string): HTMLAnchorElement => {
-  return select(`a[href*=${videoId}][id=thumbnail]`);
+  let _videoId = videoId;
+  if (hasNumberAsFirstCharPattern.test(_videoId)) {
+    _videoId = _videoId.substring(1);
+  }
+  return select(`a[href*=${_videoId}][id=thumbnail]`);
 };
